02eddac76606cb4ceb1e067d5fe14f8227331897
nettocornetto <swong496@gmail.com>
2016-10-22 02:47:38 +0800

    sad

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 module Mystore
   class Application < Rails::Application
 
-    config.assets.initialize_on_precompile = false
     config.serve_static_assets = true
 
     config.to_prepare do

d8f78b976a9b6a106706a4b75438a27c71bc09ff
nettocornetto <swong496@gmail.com>
2016-10-22 02:37:40 +0800

    sigh

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Mystore
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
 
     config.to_prepare do
       # Load application's model / class decorators
@@ -20,8 +21,6 @@ module Mystore
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
-
-    config.assets.initialize_on_precompile = false
 
 
     # Settings in config/environments/* take precedence over those specified here.
